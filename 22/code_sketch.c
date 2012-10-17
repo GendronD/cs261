24822c5199526d55f1be18d2d39a024a60e1cfa6
Jordan Bayles <jangofett481@gmail.com>
2012-10-17 11:41:00 -0700

    Finished sketching out code

--- 22/code_sketch.c
+++ 22/code_sketch.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <assert.h>
+
+#define TYPE int
 
 struct dlink
 {
@@ -11,7 +15,7 @@ struct linkedList
     struct dlink *frontSentinel;
     struct dlink *backSentinel;
     int size;
-}
+};
 
 /* The following functions were written in earlier lessons */
 void linkedListInit( struct linkedList *lst );
@@ -21,15 +25,40 @@ void _removeLink( struct linkedList *lst, struct dlink *lnk );
 
 void linkedListAdd( struct linkedList *lst, TYPE e )
 {
-    _addLink( lst, lst->frontSentinel->nest, e );
+    _addLink( lst, lst->frontSentinel->next, e );
 }
 
 int linkedListContains( struct linkedList *lst, TYPE e )
 {
+assert( lst != NULL );
+struct dlink *tmp_lnk = lst->frontSentinel;
+
+while( tmp_lnk->next != NULL )
+    {
+    if( tmp_lnk->value == e )
+        {
+        return( 1 );
+        }
+    tmp_lnk = tmp_lnk->next;
+    }
+return( 0 );
 
 }
 
 int linkedListRemove( struct linkedList *lst, TYPE e )
 {
+assert( lst != NULL );
+struct dlink *tmp_lnk = lst->frontSentinel;
+
+while( tmp_lnk->next != NULL )
+    {
+    /* We have found the link to remove */
+    if( tmp_lnk->value == e )
+        {
+        _removeLink( lst, tmp_lnk );
+        }
+    tmp_lnk = tmp_lnk->next;
+    }
+return( 0 );
 
 }
