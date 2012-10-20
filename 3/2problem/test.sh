c2f6059ee6078a289037e1c29dc64b91a424f4d1
Jordan Bayles <baylesj@flip1.engr.oregonstate.edu>
2012-10-20 12:35:32 -0700

    Collected results of running

--- 3/2problem/test.sh
+++ 3/2problem/test.sh
@@ -5,12 +5,12 @@ echo "Dynamic Array"
 for (( i = 1000; i <= 256000; i = i * 2 ))
     do
     echo $i
-    ./prog $i > dynamic_results.txt
+    ./prog $i >> dynamic_results.txt
     done
 
 echo "Linked list"
 for (( i = 1000; i <= 256000; i = i * 2 ))
     do
     echo $i
-    ../1problem/prog $i > linked_results.txt
+    ../1problem/prog $i >> linked_results.txt
     done
